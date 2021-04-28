3ddd6d03e51c28f829cb8d224bd1443b8cc48372
lediff <diffen@me.com>
2021-04-28 21:57:41 +0200

    grejjat

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Application.Activities;
 using Domain;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
@@ -11,21 +12,23 @@ namespace API.Controllers
 {
     public class ActivitiesController : BaseApiController
     {
+        private readonly IMediator _mediator;
         public ActivitiesController(IMediator mediator)
         {
+            _mediator = mediator;
 
         }
 
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            return await _context.Activities.ToListAsync();
+            return await _mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")] // söker på Id
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
-            return await _context.Activities.FindAsync(id);
+            return Ok();
         }
     }
 }
